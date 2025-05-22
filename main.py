2bb385c687b93f3a015228c87fc8076f415a41d4
tifarekaseke <t.kaseke@alustudent.com>
2025-05-23 03:48:29 +0530

    modification to currency pattern in the function

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def extract_data(your_string):
     email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
     url_pattern = r'https?://[^\s]+'
     phone_pattern = r'(\(\d{3}\) \d{3}-\d{4}|\d{3}-\d{3}-\d{4}|\d{3}\.\d{3}\.\d{4})'
-    currency_pattern = r'\$\d{1,3}(,\d{3})*(\.\d{2})?'
+    currency_pattern = r'\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?'
+
 
     emails = re.findall(email_pattern, your_string)
     urls = re.findall(url_pattern, your_string)
